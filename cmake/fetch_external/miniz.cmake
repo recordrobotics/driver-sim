60db684d3f53152267028c3068ed7bc52ce2b5a9
Compdog Inc. <compdog.inc@gmail.com>
2026-09-02 19:08:01 -0400

    Fix miniz cmake

--- cmake/fetch_external/miniz.cmake
+++ cmake/fetch_external/miniz.cmake
@@ -15,4 +15,26 @@ FetchContent_Declare(
     GIT_SHALLOW 1
     SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/miniz
 )
-FetchContent_MakeAvailable(miniz)
+FetchContent_GetProperties(miniz)
+if(NOT miniz_POPULATED)
+    FetchContent_Populate(miniz)
+endif()
+
+# Patch cmakelists
+set(_cmakelists_file "${miniz_SOURCE_DIR}/CMakeLists.txt")
+
+if(EXISTS "${_cmakelists_file}")
+    file(READ "${_cmakelists_file}" _cmakelists_contents)
+
+    # Replace min version
+    string(REPLACE
+"cmake_minimum_required(VERSION 3.0)"
+"cmake_minimum_required(VERSION 3.5)"
+        _cmakelists_contents
+        "${_cmakelists_contents}"
+    )
+
+    file(WRITE "${_cmakelists_file}" "${_cmakelists_contents}")
+endif()
+
+add_subdirectory("${miniz_SOURCE_DIR}" "${miniz_BINARY_DIR}")
