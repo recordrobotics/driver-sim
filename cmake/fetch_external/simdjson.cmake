018956fe95dbfeeaabc0c5b8ae17e5a7b5a3337d
Compdog Inc. <compdog.inc@gmail.com>
2026-09-03 11:14:39 -0400

    Update libraries

--- cmake/fetch_external/simdjson.cmake
+++ cmake/fetch_external/simdjson.cmake
@@ -7,7 +7,7 @@ set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build static libraries" FORCE)
 FetchContent_Declare(
     simdjson
     GIT_REPOSITORY https://github.com/simdjson/simdjson.git
-    GIT_TAG        v4.6.4
+    GIT_TAG        v4.6.9
     GIT_SHALLOW 1
 )
 FetchContent_MakeAvailable(simdjson)
